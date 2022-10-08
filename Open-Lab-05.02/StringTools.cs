de752ad2f6745822275955adf6fdc0b8a0e261b9
samuelmalik <samuelmalik9@gmail.com>
2022-10-08 22:02:22 +0200

    added function returning sentence without useless marks

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            while (sentence[sentence.Length - 2] == '?' || sentence[sentence.Length - 2] == '!')
+            {
+                sentence = sentence.Substring(0, sentence.Length - 1);
+            }
+            return sentence;
         }
     }
 }
